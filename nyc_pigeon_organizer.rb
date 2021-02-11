f561fa37728bbdadc9450c8b2d9c74e4b09b06fe
Sam Passarelli <passarelli.sam@gmail.com>
2021-02-11 23:13:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,38 @@
 require 'pry'
 
+# def nyc_pigeon_organizer(data)
+# organized_pigeons = data.each_with_object({}) do |(key, value), final_array|
+#   # binding.pry
+#   value.each do |inner_key, names|
+#   #  binding.pry
+#     names.each do |name|
+#       if !final_array[name]
+#         final_array[name] = {}
+#         # binding.pry
+#       end
+#       if !final_array[name][key]
+#         final_array[name][key] = []
+#       end
+#       final_array[name][key].push(inner_key.to_s)
+#         #binding.pry
+#     end
+#   end
+# end
+# binding.pry
+# organized_pigeons
+# end
+
+
 def nyc_pigeon_organizer(data)
- organized_pigeons = data.each_with_object({}) do |(key, value), final_array|
-   value.each do |inner_key, names|
-     names.each do |name|
-       binding.pry
-       if !final_array[name]
-         final_array[name] = {}
-       end
-       if !final_array[name][key]
-        final_array[name][key] = []
-       end
-       final_array[name][key].push(inner_key.to_s)
-     end
-   end
- end
+  name_hash = {}
+  data.each do |key, value|
+    value.each do |inner_key, inner_value|
+      inner_value.each do |names|
+        if !name_hash.include?(names)
+          name_hash << names
+          binding.pry
+        end
+      end
+    end
+  end
 end
